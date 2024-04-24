b3b73ab1a450e339f21c3e92850b37d608bdbcf7
Sango <60422984+MatyilaSango@users.noreply.github.com>
2024-04-24 14:52:30 +0200

    Refactor: using searchbar from react native elements

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,40 +1,36 @@
-import { StyleSheet, TextInput, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../Store/app/store'
 import { storeSearch } from '../../Store/features/search/searchSlice'
-import Ionicons from '@expo/vector-icons/Ionicons'
+import { SearchBar } from '@rneui/themed'
 
 export default function Search() {
   const search = useSelector((state: RootState) => state.search.value)
   const dispatch = useDispatch()
 
   const handleTextInput = (e: string) => {
-    dispatch(storeSearch({value: e}))
+    dispatch(storeSearch({ value: e }))
   }
 
   return (
-    <View style={styles.container}>
-      <Ionicons name="search-outline" size={30} />
-      <TextInput style={styles.textInput} defaultValue={search} placeholder='Search...' onChangeText={(e) => handleTextInput(e)}/>
-    </View>
+    <SearchBar
+      containerStyle={{ backgroundColor: "transparent" }}
+      round={true}
+      lightTheme={true}
+      style={styles.searchBar}
+      value={search}
+      placeholder='Search...'
+      onChangeText={(e) => handleTextInput(e)}
+    />
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    borderWidth: 1,
-    borderRadius: 20,
-    borderColor: "#00000050",
-    backgroundColor: "#fff",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-evenly"
-  },
-  textInput: {
-    padding: 10,
-    fontSize: 15,
+  searchBar: {
+    fontSize: 18,
     fontWeight: "600",
-    width: "90%"
+    width: "90%",
+    color: "#00000099"
   }
 })
